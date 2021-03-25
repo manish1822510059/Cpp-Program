834030bf17843f835823052a8f49107c1b5d17f6
Manish kumar <manish5854758@gmail.com>
2021-03-25 23:15:46 +0530

    Add files via upload

---
+++ Basic Program/Multiplication.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+for(int i = 1;i<=20;i++)
+{
+    for(int j = 1;j<=20;j++)
+    {
+        cout<<i<<" * "<<j<<" = "<<i * j<<endl;
+
+    }
+    cout<<endl;
+}
+
+
+}
