c50e7c144d50a75ae19d00e8b16b295088584861
Manish kumar <manish5854758@gmail.com>
2021-04-15 09:29:16 +0530

    Add files via upload

---
+++ Hacker Rank  Problem Solving/sam.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int s,n;
+    cin>>s>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(s==arr[i])
+        {
+            cout << i;
+
+        }
+    }
+
+} 
